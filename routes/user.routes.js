fcb1f4e4fbe5a74a22ad273454bfa76b4ce96a2f
Antoni Kowalewski <kowalewski.antek@gmail.com>
2022-04-02 00:47:28 +0200

    Add profile subpages and insert expose info about profile

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,12 +2,32 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/logged', (req, res) => {
-  res.render('logged');
+  console.log(req.user);
+  if(!req.user) {
+    res.redirect('/noPermission');
+  } else {
+    res.render('logged', {profilFullName: req.user.displayName, profilImage: req.user.photos[0].value});
+  }
 });
 
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
 });
 
+router.get('/profile', (req, res) => {
+  if(!req.user) {
+    res.redirect('/noPermission');
+  } else {
+    res.render('profile');
+  }
+});
+
+router.get('/profile/settings', (req, res) => {
+  if(!req.user) {
+    res.redirect('/noPermission');
+  } else {
+    res.render('settings');
+  }
+});
 
 module.exports = router;
